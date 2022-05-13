e8a8bbf1fbb7e3ed2ae1014b29a67d218f13cdb4
Matthew Bonner <matthewsbonner@comcast.net>
2022-05-13 12:43:07 -0400

    init commit

---
+++ OpenWeatherMapAPI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var apiKey = "89db265be99412b8e3d044cc91a322c5";
+            Console.WriteLine($"Welcome to this weather app.");
+            while (true)
+            {
+                Console.WriteLine($"Please enter city name:");
+                var cityName = Console.ReadLine();
+                var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+                var weatherResponse = client.GetStringAsync(weatherURL).Result;
+                Console.WriteLine(JObject.Parse(weatherResponse).GetValue("main").ToString());
+                Console.WriteLine($"Do you want to enter another city?");
+                if(Console.ReadLine().ToUpper() == "NO") { break; }
+            }
+        }
+    }
+}
